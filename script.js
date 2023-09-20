22d34583ad7098c24eda1485dbefab99afc3cb87
Shahd-ngm <143643590+Shahd-ngm@users.noreply.github.com>
2023-09-20 10:40:40 +0400

    new

---
+++ script.js
@@ -0,0 +1,6 @@
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    const ghost = document.querySelector(".ghost");
+    ghost.style.animationPlayState = "running"; // Start the animation
+});
