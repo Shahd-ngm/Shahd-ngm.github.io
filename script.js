099e2d9edc64a686f8b33e2e6f5762ed8c195a22
Shahd-ngm <143643590+Shahd-ngm@users.noreply.github.com>
2023-09-24 20:49:46 +0400

    This is my website

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 
-
+// code for the ghost 
 document.addEventListener("DOMContentLoaded", function () {
     const ghost = document.querySelector(".ghost");
     ghost.style.animationPlayState = "running"; // Start the animation
