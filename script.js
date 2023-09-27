57a35db1b16fbe402d35309a42ee4c87c9bb44be
Shahd-ngm <143643590+Shahd-ngm@users.noreply.github.com>
2023-09-27 11:16:50 +0400

    deleted files

--- script.js
+++
@@ -1,6 +0,0 @@
-
-// code for the ghost 
-document.addEventListener("DOMContentLoaded", function () {
-    const ghost = document.querySelector(".ghost");
-    ghost.style.animationPlayState = "running"; // Start the animation
-});
