d5edfae3e67a2498ffa101843332c6580e3c6306
Shahd-ngm <143643590+Shahd-ngm@users.noreply.github.com>
2023-09-27 11:19:01 +0400

    Files

---
+++ script.js
@@ -0,0 +1,6 @@
+
+// code for the ghost 
+document.addEventListener("DOMContentLoaded", function () {
+    const ghost = document.querySelector(".ghost");
+    ghost.style.animationPlayState = "running"; // Start the animation
+});
